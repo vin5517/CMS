9168db20e31845085d6edf58f5e4687d6fb54635
Vinayak Chaudhary <vinayak.chaudhary95@gmail.com>
2017-04-07 17:48:27 +0530

    Added All the Remaining Tables

--- app/src/main/java/com/carms/vinayak/cms/DatabaseHelper.java
+++ app/src/main/java/com/carms/vinayak/cms/DatabaseHelper.java
@@ -12,26 +12,81 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper{
 
     public static final String DATABASE_NAME = "CMS.db";
-    public static final String TABLE_NAME = "USER";
-    public static final String COL_1 = "FIRSTNAME";
-    public static final String COL_2 = "LASTNAME";
-    public static final String COL_3 = "DOB";
-    public static final String COL_4 = "PHONE";
+
+    // Table User
+
+    public static final String T1_TABLE_NAME = "USER";
+    public static final String T1_COL_1 = "USER_ID";
+    public static final String T1_COL_2 = "FIRSTNAME";
+    public static final String T1_COL_3 = "LASTNAME";
+    public static final String T1_COL_4 = "DOB";
+    public static final String T1_COL_5 = "PHONE";
+
+
+    //Table Vehicle
+
+    public static final String T2_TABLE_NAME = "VEHICLE";
+    public static final String T2_COL_1 = "VEHICLE_ID";
+    public static final String T2_COL_2 = "REGNO";
+    public static final String T2_COL_3 = "CHASISNO";
+    public static final String T2_COL_4 = "CARMAKE";
+    public static final String T2_COL_5 = "CARMODEL";
+    public static final String T2_COL_6 = "YEAROFMFG";
+    public static final String T2_COL_7 = "TANKCAP";
+    public static final String T2_COL_8 = "FUELAVG";
+    public static final String T2_COL_9 = "REGISTERER";
+    public static final String T2_COL_10 = "USERID";
+
+
+    //Table Expenses
+
+    public static final String T3_TABLE_NAME = "EXPENSE";
+    public static final String T3_COL_1 = "EXPENSE_ID";
+    public static final String T3_COL_2 = "DATE";
+    public static final String T3_COL_3 = "TOLL";
+    public static final String T3_COL_4 = "FUEL";
+    public static final String T3_COL_5 = "SERVICE";
+    public static final String T3_COL_6 = "PARKING";
+    public static final String T3_COL_7 = "NOTES";
+    public static final String T3_COL_8 = "VEHICLEID";
+
+
+    //Table Trip
+
+    public static final String T4_TABLE_NAME = "TRIP";
+    public static final String T4_COL_1 = "TRIP_ID";
+    public static final String T4_COL_2 = "DATE";
+    public static final String T4_COL_3 = "TRIP_TO";
+    public static final String T4_COL_4 = "TRIP_FROM";
+    public static final String T4_COL_5 = "DISTANCE";
+    public static final String T4_COL_6 = "COSTING";
+    public static final String T4_COL_7 = "NOTES";
+    public static final String T4_COL_8 = "VEHICLEID";
+
+
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
+//        SQLiteDatabase db = this.getWritableDatabase();
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_1 + " TEXT, " + COL_2 + " TEXT, " + COL_3 + " TEXT, " + COL_4 + " TEXT) ");
+        db.execSQL("CREATE TABLE " + T1_TABLE_NAME + "( " + T1_COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + T1_COL_2 + " TEXT, " + T1_COL_3 + " TEXT, " + T1_COL_4 + " TEXT, " + T1_COL_5 + " TEXT) ");
+        db.execSQL("CREATE TABLE " + T2_TABLE_NAME + "( " + T2_COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + T2_COL_2 + " TEXT, " + T2_COL_3 + " TEXT, " + T2_COL_4 + " TEXT, " + T2_COL_5 + " TEXT, " + T2_COL_6 + " TEXT, " + T2_COL_7 + " TEXT, " + T2_COL_8 + " TEXT, " + T2_COL_9 + " TEXT, " + T2_COL_10 + " INTEGER NOT NULL, FOREIGN KEY (" + T2_COL_10 + ") REFERENCES " + T1_TABLE_NAME + "(" + T1_COL_1 + ")) ");
+        db.execSQL("CREATE TABLE " + T3_TABLE_NAME + "( " + T3_COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + T3_COL_2 + " TEXT, " + T3_COL_3 + " TEXT, " + T3_COL_4 + " TEXT, " + T3_COL_5 + " TEXT, " + T3_COL_6 + " TEXT, " + T3_COL_7 + " TEXT, " + T3_COL_8 + " INTEGER NOT NULL, FOREIGN KEY (" + T3_COL_8 + ") REFERENCES " + T2_TABLE_NAME + "(" + T2_COL_1 + ")) ");
+        db.execSQL("CREATE TABLE " + T4_TABLE_NAME + "( " + T4_COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + T4_COL_2 + " TEXT, " + T4_COL_3 + " TEXT, " + T4_COL_4 + " TEXT, " + T4_COL_5 + " TEXT, " + T4_COL_6 + " TEXT, " + T4_COL_7 + " TEXT, " + T4_COL_8 + " INTEGER NOT NULL, FOREIGN KEY (" + T4_COL_8 + ") REFERENCES " + T2_TABLE_NAME + "(" + T2_COL_1 + ")) ");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + T1_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + T2_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + T3_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + T4_TABLE_NAME);
         onCreate(db);
 
     }
@@ -39,11 +94,11 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public boolean insertUser(String firstname, String lastname, String dob, String phone){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1, firstname);
-        contentValues.put(COL_2, lastname);
-        contentValues.put(COL_3, dob);
-        contentValues.put(COL_4, phone);
-        long result = db.insert(TABLE_NAME, null, contentValues);
+        contentValues.put(T1_COL_2, firstname);
+        contentValues.put(T1_COL_3, lastname);
+        contentValues.put(T1_COL_4, dob);
+        contentValues.put(T1_COL_5, phone);
+        long result = db.insert(T1_TABLE_NAME, null, contentValues);
         if (result == -1)
             return false;
         else
