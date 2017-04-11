569e9d1232666bcdb35b84e20cc92d926665e4aa
Vinayak Chaudhary <vinayak.chaudhary95@gmail.com>
2017-04-11 19:20:03 +0530

    Add Expense

---
+++ app/src/main/java/com/carms/vinayak/cms/ExpenseAdd.java
@@ -0,0 +1,13 @@
+package com.carms.vinayak.cms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpenseAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_add);
+    }
+}
